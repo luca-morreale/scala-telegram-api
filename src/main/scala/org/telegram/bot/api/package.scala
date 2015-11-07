9e4e439b18d4eae0e263308f42059858d3ac1c9f
Luca Morreale <heine.coder@gmail.com>
2015-11-07 21:30:07 +0100

    Created a generic function to extract classes from json

--- src/main/scala/org/telegram/bot/api/package.scala
+++ src/main/scala/org/telegram/bot/api/package.scala
@@ -18,9 +18,18 @@
 
 package org.telegram.bot
 
+import scala.collection.mutable.HashMap
+import scala.reflect.runtime.currentMirror
+import scala.reflect.runtime.universe.TypeTag
+import scala.reflect.runtime.universe.MethodMirror
+import scala.reflect.runtime.universe.ClassSymbol
+import scala.reflect.runtime.universe.typeOf
+import scala.reflect.runtime.universe.nme
+import scala.reflect.runtime.universe.runtimeMirror
+
 import org.json4s.DefaultFormats
-import org.json4s.JValue
 import org.json4s.JField
+import org.json4s.JValue
 
 /**
  *
@@ -34,7 +43,19 @@ package object api {
     implicit val formats = DefaultFormats
 
     /**
-     *
+     * Runtime mirror class loader
+     */
+    val mirror = runtimeMirror(getClass.getClassLoader)
+
+    /**
+     * A mutable HashMap which stores names of classes associated
+     * with its MethodMirror, that allows to instantiate the object.
+     * Used to avoid continuous interaction with reflection.
+     */
+    val classMap = HashMap[String, MethodMirror]()
+
+    /**
+     * Checks whether exits a field in the json value
      */
     def fieldExists(json: JValue, field: String): Boolean = {
         val value = json findField {
@@ -42,6 +63,45 @@ package object api {
                     case _ => false
                 }
         value != Option(None)
+    }
+
+    /**
+     * Dynamically parse the json creating the given class using reflection package.
+     */
+    def apiFromJson[API: TypeTag](json: JValue, field: String): Option[API] = {
+
+        if(fieldExists(json, field)) {
+            val generator = extractGenerator[API]()
+            Some(generator(json \ field).asInstanceOf[API])
+        } else {
+            None
+        }
+    }
+
+    /**
+     * Extracts the generator from the HashMap or creates it, in case it does not yet exits.
+     */
+    private def extractGenerator[API: TypeTag](): MethodMirror = {
+
+        val apiClass = typeOf[API].typeSymbol.asClass
+        val name = apiClass.fullName
+
+        val generator = classMap get name
+        if(generator != None) {
+            generator.get
+        } else {
+
+            val gen = createGenerator[API](apiClass)
+            classMap.put(name, gen)
+            gen
+        }
+    }
+
+    private def createGenerator[API: TypeTag](apiClass: ClassSymbol): MethodMirror = {
+        val classMirror = mirror.reflectClass(apiClass)
+        val constructor = typeOf[API].declaration(nme.CONSTRUCTOR).asMethod
+
+        classMirror.reflectConstructor(constructor)
     }
 
 }
